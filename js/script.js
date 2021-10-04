b06b607cf80848f5857568d7b847aee0943deb1f
Ivan <ivantsaryok@yandex.ru>
2021-10-04 16:54:55 +0300

    add third task

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+'use strict'
+
 // 2) Создание переменных, спрашивающих бюджет и время
 const money = +prompt("Ваш бюджет на месяц?", ""),
 	time = +prompt("Введите дату в формате YYYY-MM-DD", "");
 
-	// 3) Объект c необходимыми данным
+// 3) Объект c необходимыми данным
 const appData = {
 	budget: money,
 	timeData: time,
@@ -13,15 +15,32 @@ const appData = {
 };
 
 // 4) Вопросы пользовтелю по 2 раза и запись ответов в объект
-const firstQs1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-	secondtQs1 = prompt("Во сколько обойдется?", ""),
-	firstQs2 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-	secondtQs2 = prompt("Во сколько обойдется?", "");
-appData.expenes[firstQs1] = secondtQs1;
-appData.expenes[firstQs2] = secondtQs2;
+for (let i = 0; i < 2; i++) {
+	let firstQs = prompt("Введите обязательную статью расходов в этом месяце", ""),
+		secondtQs = +prompt("Во сколько обойдется?", "");
+	if (firstQs != "" && typeof (firstQs) == 'string' && firstQs.length < 50 && typeof (secondtQs) == 'number' && !isNaN(secondtQs)) {
+		appData.expenes[firstQs] = secondtQs;
+	} else {
+		i--;
+	}
+}
+/*
+let i = 0;
+while (i < 2) {
+	...
+	i++;
+}
+
+let i = 0;
+do {
+	...
+	i++;
+} while (i < 2);
+*/
 
 // 5) Вывод бюджета на день
-alert(`Бюджет на месяц - ${money/30}`);
+appData.moneyPerDay = appData.budget / 30;
+alert(`Бюджет на месяц - ${appData.moneyPerDay}`);
 
 // 6) Вывод полученного объекта в консоль для проверки
 console.log(appData);
