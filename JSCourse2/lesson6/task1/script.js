dba4c695220b88192eb0362daf96b8fde13e1e4f
Ivan <ivantsaryok@yandex.ru>
2022-01-04 21:37:59 +0300

    js2_lesson6

---
+++ JSCourse2/lesson6/task1/script.js
@@ -0,0 +1,6 @@
+const n = 150;
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+	sum += i;
+}
+console.log('Result: ', sum);
---
+++ JSCourse2/lesson6/task2/script.js
@@ -0,0 +1,11 @@
+const m = 10;
+const n = 20;
+let result = 1;
+
+for (let i = m; i <= n; i++) {
+	if (i % 2 !== 0) {
+		result *= i;
+	}
+}
+
+console.log('Result: ', result);
