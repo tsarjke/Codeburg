8310f9c83d52e6be1fcdf1532aa4ad8eebac863f
Ivan <ivantsaryok@yandex.ru>
2022-02-01 19:15:38 +0300

    js2_lesson29

--- JSCourse2/lesson29/task4/script.test.js
+++ JSCourse2/lesson29/task4/script.test.js
@@ -1,7 +1,6 @@
 import { compose } from './script';
 
-it('should get an array with objects which include a message with a user\'s snname and mail ', () => {
-
+it('should get one common function', () => {
 	const add2 = (value) => value + 2;
 	const square = (value) => value * value;
 	const half = (value) => value / 2;
