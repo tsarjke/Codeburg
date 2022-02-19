0a41814f92c848ae3add844a12396e96ca702b42
Ivan <ivantsaryok@yandex.ru>
2022-02-19 22:29:14 +0300

    js2_lesson32

---
+++ JSCourse2/lesson32/task1/script.js
@@ -0,0 +1,18 @@
+const requestUserData = (userId) => new Promise((resolve, reject) => {
+	if (userId === 'broken') {
+		setTimeout(() => reject(new Error('User not found')), 500);
+	} else {
+		setTimeout(() => {
+			resolve({
+				name: 'John',
+				age: 17,
+				userId,
+				email: `${userId}@example.com`,
+			});
+		}, 100);
+	}
+});
+
+requestUserData('broken')
+	.then((data) => console.log(data))
+	.catch((error) => console.log(error));
